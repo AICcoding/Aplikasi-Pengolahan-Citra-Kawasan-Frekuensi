e0c6a4ce5d739483e9afaa8bbce60482ac05902e
I Putu Agus Suarya W <agus.suarya@yahoo.co.id>
2015-12-02 21:59:43 +0800

    update DFT ke gambar serta beberapa perubahan kecil lannya...

--- Aplikasi Pengolahan Citra Kawasan Frekuensi/Form1.Designer.cs
+++ Aplikasi Pengolahan Citra Kawasan Frekuensi/Form1.Designer.cs
@@ -45,6 +45,7 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -145,7 +146,7 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(11, 19);
+            this.comboBox2.Location = new System.Drawing.Point(95, 12);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 2;
@@ -158,16 +159,16 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel2.AutoScroll = true;
             this.panel2.Controls.Add(this.pictureBox2);
-            this.panel2.Location = new System.Drawing.Point(11, 55);
+            this.panel2.Location = new System.Drawing.Point(11, 38);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(196, 144);
+            this.panel2.Size = new System.Drawing.Size(202, 160);
             this.panel2.TabIndex = 1;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Location = new System.Drawing.Point(3, 3);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(178, 124);
+            this.pictureBox2.Size = new System.Drawing.Size(178, 141);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
@@ -243,6 +244,7 @@
             // groupBox5
             // 
             this.groupBox5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.groupBox5.Controls.Add(this.button4);
             this.groupBox5.Controls.Add(this.button5);
             this.groupBox5.Controls.Add(this.button3);
             this.groupBox5.Controls.Add(this.button2);
@@ -255,41 +257,51 @@
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Control panel";
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 356);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Tentang kami";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(27, 360);
+            this.button5.Location = new System.Drawing.Point(12, 252);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(113, 22);
             this.button5.TabIndex = 8;
-            this.button5.Text = "kalkulator";
+            this.button5.Text = "Kalkulator";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(18, 160);
+            this.button3.Location = new System.Drawing.Point(9, 160);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(95, 50);
+            this.button3.Size = new System.Drawing.Size(113, 50);
             this.button3.TabIndex = 3;
-            this.button3.Text = "Tampilkan gambar hasil + DFT";
+            this.button3.Text = "DFT -> Gambar awal";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(18, 95);
+            this.button2.Location = new System.Drawing.Point(9, 105);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(95, 49);
+            this.button2.Size = new System.Drawing.Size(113, 49);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Tampilkan DFT gambar awal";
+            this.button2.Text = "Gambar awal -> DFT";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(18, 37);
+            this.button1.Location = new System.Drawing.Point(9, 25);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 38);
+            this.button1.Size = new System.Drawing.Size(113, 52);
             this.button1.TabIndex = 1;
             this.button1.Text = "Pilih gambar";
             this.button1.UseVisualStyleBackColor = true;
@@ -348,6 +360,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.GroupBox groupBox5;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Aplikasi Pengolahan Citra Kawasan Frekuensi/Form1.cs
+++ Aplikasi Pengolahan Citra Kawasan Frekuensi/Form1.cs
@@ -15,7 +15,7 @@ namespace Aplikasi_Pengolahan_Citra_Kawasan_Frekuensi
 {
     public partial class Form1 : Form
     {
-        Bitmap gambar_awal, gambar_hasil, gambar_tmp, gambar_cari_diameter, DFT_red, DFT_green, DFT_blue;
+        Bitmap gambar_awal, gambar_hasil, gambar_tmp, DFT_red, DFT_green, DFT_blue;
         Image<Bgr, Byte> gambar_awal_e, DFT_red_e, DFT_green_e, DFT_blue_e, DFT_grayscale_e, gambar_akhir_e;
 
         public Form1()
@@ -28,16 +28,18 @@ namespace Aplikasi_Pengolahan_Citra_Kawasan_Frekuensi
             comboBox4.Enabled = false;
 
             comboBox2.Text = "Pilih canel";
+            comboBox2.Items.Add("Grayscale");
             comboBox2.Items.Add("Red");
             comboBox2.Items.Add("Green");
             comboBox2.Items.Add("Blue");
-            //comboBox2.Items.Add("Grayscale");
+            
 
             comboBox4.Text = "Pilih canel";
+            comboBox4.Items.Add("Grayscale");
             comboBox4.Items.Add("Red");
             comboBox4.Items.Add("Green");
             comboBox4.Items.Add("Blue");
-            //comboBox4.Items.Add("Grayscale");
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -47,18 +49,17 @@ namespace Aplikasi_Pengolahan_Citra_Kawasan_Frekuensi
             if (pilih_gambar.ShowDialog() == DialogResult.OK)
             {
                 gambar_awal_e = new Image<Bgr, byte>(pilih_gambar.FileName);
+                DFT_grayscale_e = new Image<Bgr, byte>(pilih_gambar.FileName);
 
                 gambar_awal = new Bitmap(new Bitmap(pilih_gambar.FileName)); //gambar asli
                 gambar_hasil = new Bitmap(new Bitmap(pilih_gambar.FileName)); 
                 gambar_tmp = new Bitmap(new Bitmap(pilih_gambar.FileName)); //buat proses pengolahan
-                gambar_cari_diameter = new Bitmap(new Bitmap(pilih_gambar.FileName)); //gambar membuat lingkaran filter
 
                 DFT_red = new Bitmap(new Bitmap(pilih_gambar.FileName)); //simpan gambar DFT red
                 DFT_green = new Bitmap(new Bitmap(pilih_gambar.FileName));
                 DFT_blue = new Bitmap(new Bitmap(pilih_gambar.FileName));
 
                 pictureBox1.Image = gambar_awal_e.ToBitmap();
-                //pictureBox1.Image = gambar_awal;
 
                 button2.Enabled = true;
                 button3.Enabled = false;
@@ -75,10 +76,10 @@ namespace Aplikasi_Pengolahan_Citra_Kawasan_Frekuensi
 
             comboBox2.SelectedIndex = 0;
 
-            cari_DFT_gambar_awal_e();
+            gambarAwal_ke_DFT_e();
         }
 
-        private void cari_DFT_gambar_awal_e()
+        private void gambarAwal_ke_DFT_e()
         {
             DFT_red_e = new Image<Bgr, byte>(gambar_tmp.Width, gambar_tmp.Height);
             DFT_green_e = new Image<Bgr, byte>(gambar_tmp.Width, gambar_tmp.Height);
@@ -86,11 +87,15 @@ namespace Aplikasi_Pengolahan_Citra_Kawasan_Frekuensi
             DFT_grayscale_e = new Image<Bgr, byte>(gambar_tmp.Width, gambar_tmp.Height);
 
             Byte[,,] GetPixel_e = gambar_awal_e.Data; //Mengambil warna dari gambar awal
-            Byte[,,] SetPixel_e = DFT_red_e.Data; //Mengeset warna ke gambar akhir
+
+            Byte[, ,] SetPixelR_e = DFT_red_e.Data; //Mengeset warna ke gambar akhir
+            Byte[, ,] SetPixelG_e = DFT_green_e.Data; //Mengeset warna ke gambar akhir
+            Byte[, ,] SetPixelB_e = DFT_blue_e.Data; //Mengeset warna ke gambar akhir
+            Byte[, ,] SetPixelGS_e = DFT_grayscale_e.Data; //Mengeset warna ke gambar akhir
 
             int N, M, phi;
-            double r, g, b;
-            double gs;
+            double r, g, b, gs;
+
             N = gambar_tmp.Width;
             M = gambar_tmp.Height;
             phi = 180;
@@ -107,17 +112,12 @@ namespace Aplikasi_Pengolahan_Citra_Kawasan_Frekuensi
                     {
                         for (int y = 0; y < N; y++)
                         {
-                            //gs += (((float)GetPixel_e[x, y, 0] + (float)GetPixel_e[x, y, 1] + (float)GetPixel_e[x, y, 2]) / 3F) * Math.Cos(2 * phi * (((float)u * (float)x / (float)N) + ((float)v * (float)y / (float)M)));
                             gs += (((float)GetPixel_e[x, y, 0] + (float)GetPixel_e[x, y, 1] + (float)GetPixel_e[x, y, 2]) / 3F) * cos(2 * phi * (((float)u * (float)x / (float)M) + ((float)v * (float)y / (float)N)));
-
-                            /*r += GetPixel_e[y, x, 2] * Math.Cos(2 * phi * ((u * x / N) + (v * y / M)));
-                            g += GetPixel_e[y, x, 1] * Math.Cos(2 * phi * ((u * x / N) + (v * y / M)));
-                            b += GetPixel_e[y, x, 0] * Math.Cos(2 * phi * ((u * x / N) + (v * y / M)));*/
+                            r += ((float)GetPixel_e[x, y, 2]) * cos(2 * phi * (((float)u * (float)x / (float)M) + ((float)v * (float)y / (float)N)));
+                            g += ((float)GetPixel_e[x, y, 1]) * cos(2 * phi * (((float)u * (float)x / (float)M) + ((float)v * (float)y / (float)N)));
+                            b += ((float)GetPixel_e[x, y, 0]) * cos(2 * phi * (((float)u * (float)x / (float)M) + ((float)v * (float)y / (float)N)));
                         }
-                    }
-                    /*r /= M / N;
-                    g /= M / N;
-                    b /= M / N;
+                    }                
 
                     if (r > 255)
                         r = 255;
@@ -132,22 +132,31 @@ namespace Aplikasi_Pengolahan_Citra_Kawasan_Frekuensi
                     if (b > 255)
                         b = 255;
                     else if (b < 0)
-                        b = 0;*/
-                    //MessageBox.Show(gs.ToString());
-                    //gs = (1F / M * N) * gs;
+                        b = 0;
 
                     if (gs < 0)
                         gs = 0;
                     else if (gs > 255)
                         gs = 255;
 
+                    SetPixelR_e[u, v, 0] = 0;
+                    SetPixelR_e[u, v, 1] = 0;
+                    SetPixelR_e[u, v, 2] = (byte)r;
 
-                    SetPixel_e[u, v, 0] = (byte)gs;
-                    SetPixel_e[u, v, 1] = (byte)gs;
-                    SetPixel_e[u, v, 2] = (byte)gs;               
+                    SetPixelG_e[u, v, 0] = 0;
+                    SetPixelG_e[u, v, 1] = (byte)g;
+                    SetPixelG_e[u, v, 2] = 0;
+
+                    SetPixelB_e[u, v, 0] = (byte)b;
+                    SetPixelB_e[u, v, 1] = 0;
+                    SetPixelB_e[u, v, 2] = 0;
+
+                    SetPixelGS_e[u, v, 0] = (byte)gs;
+                    SetPixelGS_e[u, v, 1] = (byte)gs;
+                    SetPixelGS_e[u, v, 2] = (byte)gs;             
                 }
             }
-            pictureBox2.Image = DFT_red_e.ToBitmap();
+            pictureBox2.Image = DFT_grayscale_e.ToBitmap();
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -156,67 +165,43 @@ namespace Aplikasi_Pengolahan_Citra_Kawasan_Frekuensi
 
             comboBox2.Enabled = true;
             comboBox4.Enabled = true;
-            DFT_ke_gambar_e();
+            DFT_ke_gambarAwal_e();
         }
 
-        private void DFT_ke_gambar_e()
+        private void DFT_ke_gambarAwal_e()
         {
             gambar_akhir_e = new Image<Bgr, byte>(gambar_tmp.Width, gambar_tmp.Height);
 
-            Byte[, ,] GetPixel_e = DFT_red_e.Data; //Mengambil warna dari gambar awal
-            Byte[, ,] SetPixel_e = gambar_akhir_e.Data; //Mengeset warna ke gambar akhir
+            Byte[, ,] GetPixel_e = DFT_grayscale_e.Data; //Mengambil warna dari gambar awal
+
+            Byte[, ,] SetPixel_e = gambar_akhir_e.Data; //Mengeset warna ke gambar akhir       
 
             int N, M, phi;
-            double r, g, b;
             double gs;
+
             N = gambar_tmp.Width;
             M = gambar_tmp.Height;
             phi = 180;
 
-            for (int u = 0; u < gambar_tmp.Height; u++)
+            for (int u = 0; u < M; u++)
             {
-                for (int v = 0; v < gambar_tmp.Width; v++)
+                for (int v = 0; v < N; v++)
                 {
-                    r = 0;
-                    g = 0;
-                    b = 0;
                     gs = 0;
-                    for (int x = 0; x < gambar_tmp.Height; x++)
+                    for (int x = 0; x < M; x++)
                     {
-                        for (int y = 0; y < gambar_tmp.Width; y++)
+                        for (int y = 0; y < N; y++)
                         {
-                            gs += (((float)GetPixel_e[x, y, 0] + (float)GetPixel_e[x, y, 1] + (float)GetPixel_e[x, y, 2]) / 3F) * cos(2 * phi * (((float)u * (float)x / (float)N) + ((float)v * (float)y / (float)M)));
-
-                            /*r += GetPixel_e[y, x, 2] * Math.Cos(2 * phi * ((u * x / N) + (v * y / M)));
-                            g += GetPixel_e[y, x, 1] * Math.Cos(2 * phi * ((u * x / N) + (v * y / M)));
-                            b += GetPixel_e[y, x, 0] * Math.Cos(2 * phi * ((u * x / N) + (v * y / M)));*/
+                            gs += (((float)GetPixel_e[x, y, 0] + (float)GetPixel_e[x, y, 1] + (float)GetPixel_e[x, y, 2]) / 3F) * cos(2 * phi * (((float)u * (float)x / (float)M) + ((float)v * (float)y / (float)N)));
                         }
                     }
-                    /*r /= M / N;
-                    g /= M / N;
-                    b /= M / N;
 
-                    if (r > 255)
-                        r = 255;
-                    else if (r < 0)
-                        r = 0;
+                    gs = (1F / M * N) * gs;
 
-                    if (g > 255)
-                        g = 255;
-                    else if (g < 0)
-                        g = 0;
-
-                    if (b > 255)
-                        b = 255;
-                    else if (b < 0)
-                        b = 0;*/
-                    //MessageBox.Show(gs.ToString());
-                    gs = gs * (1F / (float)M * (float)N);
                     if (gs < 0)
                         gs = 0;
                     else if (gs > 255)
                         gs = 255;
-
 
                     SetPixel_e[u, v, 0] = (byte)gs;
                     SetPixel_e[u, v, 1] = (byte)gs;
@@ -230,21 +215,24 @@ namespace Aplikasi_Pengolahan_Citra_Kawasan_Frekuensi
         {
             if(comboBox2.SelectedIndex == 0)
             {
-                pictureBox2.Image = DFT_red;
+                pictureBox2.Image = DFT_grayscale_e.ToBitmap();
             }
             else if (comboBox2.SelectedIndex == 1)
             {
-                pictureBox2.Image = DFT_green;
+                pictureBox2.Image = DFT_red_e.ToBitmap();
             }
             else if (comboBox2.SelectedIndex == 2)
             {
-                pictureBox2.Image = DFT_blue;
+                pictureBox2.Image = DFT_green_e.ToBitmap();
+            }
+            else if (comboBox2.SelectedIndex == 3)
+            {
+                pictureBox2.Image = DFT_blue_e.ToBitmap();
             }
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(cos(90).ToString());
             kalkulator a = new kalkulator();
             a.Show();
         }
@@ -256,6 +244,14 @@ namespace Aplikasi_Pengolahan_Citra_Kawasan_Frekuensi
             hasil = Math.Cos(tmp);
             hasil = Math.Round(hasil,4);
             return hasil;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {        
+            MessageBox.Show("Dibuat Oleh :\n\n1. I Wayan Ariantha Sentanu\t[1308605009]\n2. I Gede Pramarta Sedana\t[1308605027]\n3. I Putu Agus Suarya Wibawa\t[1308605034]\n4. Daniel Kurniawan\t\t[1308605039]",
+                                  "Tentang Kami", MessageBoxButtons.OK,
+                                  MessageBoxIcon.Information,
+                                  0);
         }
     }
 }
